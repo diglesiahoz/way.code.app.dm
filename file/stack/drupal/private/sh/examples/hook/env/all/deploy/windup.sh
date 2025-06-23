51fa10b2e8d2d1248f75a4c5a780b408ea873e9e
Daniel De La Iglesia <diglesia@opentrends.net>
2025-06-23 13:37:10 +0200

    Changes

--- file/stack/drupal/private/sh/examples/hook/env/all/deploy/windup.sh
+++ file/stack/drupal/private/sh/examples/hook/env/all/deploy/windup.sh
@@ -2,7 +2,7 @@
 
 if [ "$APPSETTING_NOTIFY_ENABLE" = true ]
 then
-  cmd "timeout 5 /usr/bin/swaks --to $APPSETTING_NOTIFY_EMAIL --header \"Subject: Deploy ${APPSETTING_ENV^^} ${EXIT_STATUS^^}\" --body \"$EXIT_MESSAGE\" > /dev/null"
+  cmd "timeout 5 /usr/bin/swaks --to $APPSETTING_NOTIFY_EMAIL --header \"Subject: Deployment ${EXIT_STATUS^^} from ${APPSETTING_ENV^^}\" --body \"$EXIT_MESSAGE\" > /dev/null"
   checkError "The notification could not be sent."
 else
   log "Email notifications disabled"
